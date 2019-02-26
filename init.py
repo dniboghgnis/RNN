68abb5d15f78e3ba22ca3bfcaab32eb60f8e3ad1
Gobind Singh <withgobind@gmail.com>
2019-02-26 14:56:40 +0530

    Create init.py

---
+++ init.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+
+
+num_epochs=100
+total_series_length=50000
+truncated_backdrop_length=15
+state_size=4
+num_classes=2
+echo_step=3
+batch_size=5
+num_batches=total_series_length//batch_size//truncated_backdrop_length
+
+
+print(20//5//2)
+
+
