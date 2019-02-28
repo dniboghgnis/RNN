2422af8cff9c79632d888f98040fc526fc8e56e9
Gobind Singh <withgobind@gmail.com>
2019-03-01 01:58:55 +0530

    initial codes

--- init.py
+++ init.py
@@ -13,8 +13,8 @@ num_classes=2
 echo_step=3
 batch_size=5
 num_batches=total_series_length//batch_size//truncated_backdrop_length
+# print(20/2/2)
 
-
-print(20//5//2)
-
-
+# def gen_data():
+x=np.array(np.random.choice(4,1,p=[0.25,0.25,0.25,0.25]))
+print(x)
